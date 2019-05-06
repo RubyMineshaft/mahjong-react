60fda3830d1c18e8c9012b75f97d34e6e626d504
mattetress <mattetress@me.com>
2019-05-05 23:28:23 -0500

    add routes for users and login/out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope :api do
-    get '/login' => 'application#index'
+    post '/login' => 'sessions#create'
+    delete '/logout' => 'sessions#destroy'
+    get '/profile' => 'users#profile'
+    resources :users 
   end
 
 end
